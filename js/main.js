4284c8ec3109971762c265fcc003d4e4e2299361
Andy Burke <aburke@bitflood.org>
2010-10-22 14:29:31 -0700

    - handle youtube embedding

--- js/main.js
+++ js/main.js
@@ -36,18 +36,47 @@ var MessageLinkifier = function()
     
     this.HandleMessage = function( msg )
     {
+        if ( msg.contentLinkified )
+        {
+            return;
+        }
+
         msg.content = linkify( msg.content );
+        msg.contentLinkified = true;
     };
 };
 
 var YoutubeHandler = function()
 {
     this.types = [ 'message' ];
-    this.priority = 0;
-    
+    this.priority = 100;
+
+    // via stackoverflow
+    var vidWidth = 480;
+    var vidHeight = 360;
+   
+    // Tthis could be done by creating an object, adding attributes & inserting parameters, but this is quicker
+    var e1 = '<obj'+'ect width="' + vidWidth + '" height="' + vidHeight + '"><param name="movie" value="http://www.youtube.com/v/';
+    var e2 = '&hl=en&fs=1"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/';
+    var e3 = '&hl=en&fs=1" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="' + vidWidth + '" ' + 'height="' + vidHeight + '"></embed></object> ';
+
     this.HandleMessage = function( msg )
     {
-        //embed youtube links
+        if ( msg.contentLinkified )
+        {
+            return;
+        }
+        
+        var vid = msg.content.match(/((\?v=)(\w[\w|-]*))/g); // end up with ?v=oHg5SJYRHA0
+        if ( vid && vid.length )
+        {
+            $.each( vid, function(i) {
+                var ytid = this.replace(/\?v=/,'') // end up with oHg5SJYRHA0
+                msg.content = e1 + ytid + e2 + ytid + e3;
+                msg.contentLinkified = true;
+                msg.rawHTML = true;
+            })
+        }
     };
 };
 
@@ -117,7 +146,7 @@ var MessageRenderer = function()
     
     this.HandleMessage = function( msg )
     {
-        msg.content = ( typeof( msg.content ) == 'undefined' || msg.content == null ) ? '' : htmlEscape( msg.content );
+        msg.content = ( typeof( msg.content ) == 'undefined' || msg.content == null ) ? '' : ( msg.rawHTML ? msg.content : htmlEscape( msg.content ) );
         msg.friendlyTimestamp = new Date( msg.timestamp ).format( timestamp_display_format );
 
         var render = true;
