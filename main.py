c623702542bd345514f5f1aaa5a407f7679f071b
Sergey Basharov <sergey@skible.com>
2011-03-19 14:55:21 +0300

    Removed redundant parentheses

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import wsgiref.handlers
 
-
 from google.appengine.ext import webapp
 from utils import *
 
 
 class MainHandler(webapp.RequestHandler):
+    def get(self):
+        account = get_account()
+        if account:
+            self.redirect('/room/')
+        else:
+            self.redirect('/account/')
 
-  def get(self):
-    account = get_account()
-    if (account):
-        self.redirect('/room/')
-    else:
-        self.redirect('/account/')
 
 def main():
-  application = webapp.WSGIApplication([('/', MainHandler)],
-                                       debug=True)
-  wsgiref.handlers.CGIHandler().run(application)
+    application = webapp.WSGIApplication([('/', MainHandler)],
+                                         debug=True)
+    wsgiref.handlers.CGIHandler().run(application)
 
 
 if __name__ == '__main__':
-  main()
+    main()
